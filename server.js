7b16c19f9067cf0a5b71afd64e0781a0ab6b6166
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-06-13 22:24:10 +0300

    Some fixed

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
-// app.js
+
 
 const express = require('express');
 const path = require('path');
 const app = express();
 
-// EJS view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Sample data (assuming ilceler array)
 const ilceler = require("./ilceler.json");
 
-// Route for the home page
+
 app.get('/', (req, res) => {
-    res.render('index', { ilceler }); // Render 'index.ejs' and pass ilceler data
+    res.render('index', { ilceler }); 
 });
-// { "id": "1", "sehir_id": "37", "ilce_adi": "Abana" 
-// Route for search
+
+
 app.get('/search', (req, res) => {
-    const plaka = req.query.plaka; // Get search query parameter
+    const plaka = req.query.plaka; 
     
     const filteredIlceler = ilceler.filter(ilce =>
         ilce.sehir_id === plaka
@@ -27,10 +25,9 @@ app.get('/search', (req, res) => {
     res.json(filteredIlceler);
 });
 
-// Static files middleware (CSS, images, etc.)
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
